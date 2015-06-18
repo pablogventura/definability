804115456e215e4e8c1e2c4724daa9cbff788a04
pablogventura <pablogventura@gmail.com>
2015-06-18 17:02:02 -0300

    no devel

--- devel/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-uapth = 'ua-'
-clspth = os.getenv('SAGE_LOCAL') + '/bin/'
-datapth = os.getenv('SAGE_ROOT') + '/data/univ_algebra/'
-
-minionpath = "" # vacio para el predeterminado
-
