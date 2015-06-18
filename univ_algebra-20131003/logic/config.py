e44aa966723009b141ba50ff10eeb526768c5bc1
pablogventura <pablogventura@gmail.com>
2015-06-18 16:37:20 -0300

    config.py

---
+++ univ_algebra-20131003/logic/config.py
@@ -0,0 +1,9 @@
+import os
+
+
+uapth = 'ua-'
+clspth = os.getenv('SAGE_LOCAL') + '/bin/'
+datapth = os.getenv('SAGE_ROOT') + '/data/univ_algebra/'
+
+minionpath = "" # vacio para el predeterminado
+
