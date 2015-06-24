e9495bb0f847cdc1f04d774050d650ab91e1b42f
pablogventura <pablogventura@gmail.com>
2015-06-24 12:37:10 -0300

    comentario a datapth

--- univ_algebra-20131003/logic/config.py
+++ univ_algebra-20131003/logic/config.py
@@ -6,7 +6,7 @@ home = os.getenv('HOME')
 print os.getenv('SAGE_LOCAL')
 print clspth
 print home
-datapth = os.path.join(os.getenv('SAGE_ROOT'),'data/univ_algebra/')
+datapth = os.path.join(os.getenv('SAGE_ROOT'),'data/univ_algebra/') # solo se usa en readlistfromfile(st), no queda clara la utilidad
 
 minionpath = os.path.join(home,"minion-1.8/bin/") # siempre tienen que terminar en barra
 ladrpath = os.path.join(home,"LADR-2009-11A/bin/")
