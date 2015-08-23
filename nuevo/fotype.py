4744429e6c31fcd1f6cef5568d3e361a75475532
pablogventura <pablogventura@gmail.com>
2015-08-23 03:04:17 -0300

    maneja subtipos

--- nuevo/fotype.py
+++ nuevo/fotype.py
@@ -10,12 +10,27 @@ class FO_Type(object):
     >>> t = FO_Type({"+":2},{"<":2})
     >>> t
     FO_Type({'+': 2},{'<': 2})
+    >>> st = t.subtype(["+"],[])
+    >>> st
+    FO_Type({'+': 2},{},supertype=FO_Type({'+': 2},{'<': 2}))
+    >>> st.is_subtype_of(t)
+    True
     """
-    def __init__(self,operations,relations):
+    def __init__(self,operations,relations,supertype=None):
         self.operations = operations
         self.relations = relations
+        self.supertype = supertype
     def __repr__(self):
-        return "FO_Type(%s,%s)"%(repr(self.operations),repr(self.relations))
+        if self.supertype:
+            return "FO_Type(%s,%s,supertype=%s)"%(repr(self.operations),repr(self.relations),repr(self.supertype))
+        else:
+            return "FO_Type(%s,%s)"%(repr(self.operations),repr(self.relations))
+    def __eq__(self,other):
+        return self.operations == other.operations and self.relations == other.relations
+    def subtype(self,operations,relations):
+        return FO_Type({op:self.operations[op] for op in operations},{rel:self.relations[rel] for rel in relations},supertype=self)
+    def is_subtype_of(self, supertype):
+        return self.supertype == supertype
 
 if __name__ == "__main__":
     import doctest
