8381ecd49f03355d02f67eb6e5453ce88f2e583a
Pablo Ventura <pablogventura@gmail.com>
2015-09-06 19:24:31 -0300

    mejorando multigrafo

--- fundict/constellation.py
+++ fundict/constellation.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
-
 from networkx import MultiDiGraph
 from examples import *
 from collections import defaultdict
 
-
-class Constellation(object):
+class TipedMultiDiGraph(object):
     """
     Maneja una coleccion de modelos relacionados por flechas
     """
@@ -21,27 +19,47 @@ class Constellation(object):
         self.graph.add_node(planet)
 
     def add_satellite(self, satellite, inclusion, planet):
-        #agrega un satellite y por una funcion de inclusion
+        #agrega un satellite y una funcion de inclusion
         self.satellite[len(satellite)].append(satellite)
-        self.graph.add_node(planet)
-
-    def generate(self,subtype):
-        """
-        Ordeno planetas por tamaño de mayor a menor
-        Recorro subestructuras de menor a mayor haciendo 'lo de siempre',
-            si son del mismo tamaño que que un planeta no revisado me fijo si es isomorfo y preserva*, y en ese caso se borra ese planeta
-                * es muy posible que no haga falta revisar si preserva ya que es isomorfo, y mediante automorfismos de la original basta.
-        Lo de siempre:
-            Revisar automorfismos de la subestructrura, y revisar un solo isomorfismo contra las subestructuras isomorfas.
-        """
-    def ppnetwork(na,archivo):
-        G = nx.DiGraph()
-        G.add_edges_from([tuple(k) for k in na])
-
+        self.graph.add_node(satellite)
+        self.add_arrow(inclusion)
+    
+    def add_arrow(self,arrow):
+        assert self.graph.has_node(arrow.source) and self.graph.has_node(arrow.target)
+        duplicate=self.__find_arrow(arrow)
+        if duplicate:
+            print "esa flecha ya estaba! capaz con otro tipo?"
+        
+        self.graph.add_edge(arrow.source,arrow.target,arrow=arrow)
+    
+    def add_arrows(self,arrows):
+        for arrow in arrows:
+            self.add_arrow(arrow)
+            
+    def __find_arrow(self,arrow):
+        result=self.find_arrows(arrow.source,arrow.target)
+        result=filter(lambda x: x==arrow,result)
+        return result
+        
+    def find_arrows(self, source, target, morphtype=None):
+        if self.graph.has_edge(source,target):
+            result = self.graph[source][target].values()
+            result = map(lambda x: x["arrow"],result)
+            if morphtype:
+                result = filter(lambda x: isinstance(x,morphtype),result)
+            return result
+        else:
+            return []
+        
+        
+    def show(self):
+        import networkx as nx
+        G = self
+        archivo = "constellation"
         pos=nx.graphviz_layout(G,prog='dot',args='')
         plt.figure(figsize=(8,8))
-        nombres = {tuple(k):back(na[k][1]) for k in na}
-        edge_labels=nx.draw_networkx_edge_labels(G,pos,edge_labels=nombres,label_pos=0.8)
+        #nombres = {tuple(k):back(na[k][1]) for k in na}
+        #edge_labels=nx.draw_networkx_edge_labels(G,pos,edge_labels=nombres,label_pos=0.8)
         nx.draw(G,pos,node_size=200,alpha=1,node_color="white", with_labels=True)
         plt.axis('equal')
         plt.savefig('%s.png' % archivo)
