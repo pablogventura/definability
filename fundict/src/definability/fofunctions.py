f166f49c45d6ccfe9c1d11a68abd7f8e9341da90
Pablo Ventura <pablogventura@gmail.com>
2015-11-09 15:18:43 -0300

    se puede revisar si una relacion es el grafico de una funcion

--- fundict/src/definability/fofunctions.py
+++ fundict/src/definability/fofunctions.py
@@ -35,8 +35,21 @@ class FO_Relation(FO_OpRel):
         super(FO_Relation, self).__init__(d)
         self.relation = True
 
-
-
+    def is_a_function_graph(self, universe):
+        """
+        Revisa si la relacion es el grafico de una funcion para un universo dado.
+        Si lo es, devuelve la funcion, y sino devuelve False.
+        """
+        table = self.table()
+        function = filter(lambda t: all(v in universe for v in t), table) # filtra los que estan en el universo
+        function = {tuple(t[:(self.arity()-1)]):t[-1] for t in function} # le quita el ultimo a cada tupla
+        sdomain = set(map(tuple,function.keys()))
+        if len(function) != len(sdomain): # habia tuplas repetidas, no respeta la condicion de funcion
+            return False
+        elif len(sdomain) != len(universe)**(self.arity()-1): # no tiene dominio suficiente
+            return False
+        else:
+            return FO_Operation(function)
 
 if __name__ == "__main__":
     import doctest
