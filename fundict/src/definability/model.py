e9e965cf58795394d97cc7bd751fc726b1a6401a
Pablo Ventura <pablogventura@gmail.com>
2015-11-14 17:52:38 -0300

    model no usaba fo_operation

--- fundict/src/definability/model.py
+++ fundict/src/definability/model.py
@@ -5,7 +5,7 @@ from itertools import product
 
 from misc import indent, powerset
 from morphisms import Embedding
-from fofunctions import FO_Relation, FO_Operation
+from fofunctions import FO_Relation
 import minion
 
 
