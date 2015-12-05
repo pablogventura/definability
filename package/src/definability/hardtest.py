78eb18945c19f82f0b2d3da751ac2cbbc46aebdd
Pablo Ventura <pablogventura@gmail.com>
2015-12-05 19:48:25 -0300

    agrego test que prueba con todos los grafos dirigidos de cardinalidad 5

---
+++ package/src/definability/hardtest.py
@@ -0,0 +1,27 @@
+import config
+import examples
+import files
+import fofunctions
+import fotheories
+import fotype
+import functions
+import minion
+import misc
+import model
+import morphisms
+import constellation
+import lindenbaum
+import preorder
+from examples import *
+from morphisms import *
+
+grafos = fotheories.DiGraph.find_models(5)
+for g in grafos:
+    c=constellation.Constellation(g)
+    atomos = lindenbaum.atoms_of_existencial_definable_algebra(c, g.fo_type, 3)
+    jis = lindenbaum.ji_of_existencial_positive_definable_algebra(c, g.fo_type, 3)
+    jjis = lindenbaum.new_ji_of_existencial_positive_definable_algebra(atomos,c, g.fo_type)
+    assert len(jis) == len(jjis[0])
+    print "*" * 80
+    print g
+    print "*" * 80
