8a90624c6c1fae9c5beff33cab80e66a418e5c58
Pablo Ventura <pablogventura@gmail.com>
2016-02-17 17:14:55 -0300

    mensaje con los paths de minion y ladr

--- package/src/setup.py
+++ package/src/setup.py
@@ -2,6 +2,17 @@
 
 from distutils.core import setup
 
+from definability import config
+
+print "*" * 80
+print ""
+print "This package needs Minion and LADR."
+print "Minion path: %s" % config.minion_path
+print "LADR path: %s" % config.ladr_path
+print "If you need to change the path, edit config.py and reinstall."
+print ""
+print "*" * 80
+
 setup(name='definability',
       version='0.1',
       description='',
