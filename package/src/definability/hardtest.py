c514d53119ec6cf6cf36bd8d8662a7779ae09097
Pablo Ventura <pablogventura@gmail.com>
2016-02-18 09:48:35 -0300

    limpiando

--- package/src/definability/hardtest.py
+++ package/src/definability/hardtest.py
@@ -20,8 +20,6 @@ for g in grafos:
     c=constellation.Constellation(g)
     atomos = lindenbaum.ji_of_existencial_definable_algebra(c, g.fo_type, 3)
     jis = lindenbaum.ji_of_existencial_positive_definable_algebra(c, g.fo_type, 3)
-    jjis = lindenbaum.new_ji_of_existencial_positive_definable_algebra(atomos,c, g.fo_type)
-    assert len(jis) == len(jjis[0])
     print "*" * 80
     print g
     print "*" * 80
