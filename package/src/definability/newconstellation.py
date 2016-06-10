9cec2e9874ee6149f4a9ae1f37e0a8c570667e43
Pablo Ventura <pablogventura@gmail.com>
2016-06-10 12:25:37 -0300

    nuevo algoritmo para definibilidad abierta

--- package/src/definability/newconstellation.py
+++ package/src/definability/newconstellation.py
@@ -3,7 +3,7 @@
 import networkx
 from examples import *
 from collections import defaultdict
-from itertools import product, chain, combinations
+from itertools import product, chain, combinations, ifilter
 from morphisms import Isomorphism, Embedding, Homomorphism
 from minion import ParallelMorphMinionSol
 from model import FO_Model
@@ -16,19 +16,74 @@ def preprocessing(k,subtype,supertype):
     >>> k= {rettest10, rettest102}
     >>> len(k)
     2
-    >>> len(preprocessing(k,tiporet, tiporet + tipoposet))
-    1
+    >>> k,ce = preprocessing(k,tiporet, tiporet + tipoposet)
+    >>> (len(k),ce)
+    (1, None)
     """
     for (a,b) in combinations(k,2): # nunca con si mismo
-        if {a,b} <= k: # si ya lo encontre iso no lo busco de nuevo
+        if {a,b} <= k: # si ya lo encontre isomorfo, no lo busco de nuevo
             iso = a.is_isomorphic(b,subtype)
-            if iso and iso.preserves_type(supertype):
-                k.remove(b)
+            if iso:
+                if iso.preserves_type(supertype):
+                    k.remove(b)
+                else:
+                    return (k,iso)
+    return (k,None)
+
+def is_open_definable(k, subtype, supertype):
+    """
+    Devuelve una tupla diciendo si es definible y un contrajemplo
+    para la definibilidad abierta de supertype, en k con el subtype
+
+    >>> from examples import *
+    >>> k = {retrombo}
+    >>> is_open_definable(k,tiporet,tiporet+tipoposet)
+    (True, None)
+    >>> is_open_definable(k,tiporet,tiporet+tipotest)
+    (False, Isomorphism(
+      [2] -> 0,
+    ,
+      FO_Type({'v': 2, '^': 2},{})
+    ,
+      antitype= ['P']
+    ,
+      Injective,
+      Surjective,
+    ))
+    """
+    k,ce=preprocessing(k,subtype,supertype) # hago el preprosesamiento
+    if ce:
+        #encontre un contraejemplo durante el preprosesamiento
+        return (False,ce)
+    s=set()
+    for a in sorted(k, key=len, reverse=True):
+        for (i,b) in a.substructures(subtype): # hay que chequear que las devuelva de mayor a menor
+            # i es una funcion de inclusion, no se si hace falta
+            iso, ce = check_isos(b,s,subtype,supertype)
+            if ce:
+                # ce es un isomorfismo contraejemplo
+                return (False, ce)
+            elif iso:
+                pass
+                #TODO filtrar subestructuras de esta subestrucutra
             else:
-                return iso
-    return k
+                s.add(b)
+                for aut in b.automorphisms(subtype):
+                    if not aut.preserves_type(supertype):
+                        # aut es un automorfismo contrajemplo
+                        return (False, aut)
+    return (True,None)
 
-
+def check_isos(a,s,subtype,supertype):
+    for b in ifilter(lambda x:len(a)==len(x),s):
+        iso = a.is_isomorphic(b,subtype)
+        if iso:
+            if iso.preserves_type(supertype):
+                return (True,None)
+            else:
+                #iso es un contraejemplo
+                return (True,iso)
+    return (False,None)
 
 if __name__ == "__main__":
     import doctest
