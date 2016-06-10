8fd02ed3238711f66522f8823c1e3f2345c6ac99
Pablo Ventura <pablogventura@gmail.com>
2016-06-10 12:53:29 -0300

    casi listo el nuevo algoritmo para abiertas positivas

--- package/src/definability/newconstellation.py
+++ package/src/definability/newconstellation.py
@@ -85,6 +85,56 @@ def check_isos(a,s,subtype,supertype):
                 return (True,iso)
     return (False,None)
 
+def is_open_positive_definable(k, subtype, supertype):
+    """
+    Devuelve una tupla diciendo si es definible y un contrajemplo
+    para la definibilidad abierta positiva de supertype, en k con el subtype
+
+    >>> from examples import *
+    >>> k = {retrombo}
+    >>> is_open_positive_definable(k,tiporet,tiporet+tipoposet)
+    (True, None)
+    >>> is_open_positive_definable(k,tiporet,tiporet+tipotest)
+    (False, Isomorphism(
+      [2] -> 0,
+    ,
+      FO_Type({'v': 2, '^': 2},{})
+    ,
+      antitype= ['P']
+    ,
+      Injective,
+      Surjective,
+    ))
+    """
+    k,ce=preprocessing(k,subtype,supertype) # hago el preprosesamiento
+    if ce:
+        #encontre un contraejemplo durante el preprosesamiento
+        return (False,ce)
+    s=set()
+    for a in sorted(k, key=len, reverse=True):
+        for (i,b) in a.substructures(subtype): # hay que chequear que las devuelva de mayor a menor
+            # i es una funcion de inclusion, no se si hace falta
+            iso, ce = check_isos(b,s,subtype,supertype) # TODO BIHOMOS
+            if ce:
+                # ce es un homomorfismo biyectivo o isomorfismo contraejemplo
+                return (False, ce)
+            elif iso:
+                pass
+                #TODO filtrar subestructuras de esta subestrucutra
+            else:
+                s.add(b)
+                for aut in b.automorphisms(subtype):
+                    if not aut.preserves_type(supertype):
+                        # aut es un automorfismo contrajemplo
+                        return (False, aut)
+    for a,b in ifilter(lambda (a,b): len(a)>len(b), combinations(s,2)):
+        # como len(a)>len(b) los homos sobre no son inyectivos
+        for homo in a.homomorphisms_to(b, subtype, surj=True):
+            if not homo.preserves_type(supertype):
+                # homo es un homomorfismo sobreyectivo no inyectivo contraejemplo
+                return (False,homo)
+    return (True,None)
+
 if __name__ == "__main__":
     import doctest
     doctest.testmod()
