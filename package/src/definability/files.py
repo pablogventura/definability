9e43c340d0dbf5273df366004a991b52d35ec884
Pablo Ventura <pablogventura@gmail.com>
2016-06-20 21:06:35 -0300

    tests

--- package/src/definability/files.py
+++ package/src/definability/files.py
@@ -6,13 +6,31 @@ import pickle
 
 
 def object_to_file(obj, path):
-    f = file(path, "a")
+    """
+    Guarda un objeto en un archivo
+    
+    >>> object_to_file([1,3],"testfile")
+    >>> l = file_to_object("testfile")
+    >>> l
+    [1, 3]
+    >>> remove("testfile")
+    """
+    f = open(path, "wb")
     pickle.dump(obj, f)
     f.close()
 
 
 def file_to_object(path):
-    f = file(path, "r")
+    """
+    Lee un objeto en un archivo
+    
+    >>> object_to_file([1,3],"testfile")
+    >>> l = file_to_object("testfile")
+    >>> l
+    [1, 3]
+    >>> remove("testfile")
+    """
+    f = open(path, "rb")
     obj = pickle.load(f)
     f.close()
     return obj
@@ -21,6 +39,7 @@ def file_to_object(path):
 def create_pipe(path):
     """
     Crea un named pipe
+    
     """
     try:
         os.mkfifo(path)
@@ -30,18 +49,14 @@ def create_pipe(path):
 
 
 def remove(path):
+    """
+    Elimina un archivo
+    """
     try:
         os.remove(path)
     except OSError:
         # no existe
         pass
-
-
-def read(path):
-    f = open(path)
-    data = f.read()
-    f.close()
-    return data
 
 
 def write(path, data):
