47847bb1654f549182a67783733da0e81b8f26e7
Pablo Ventura <pablogventura@gmail.com>
2016-06-24 17:02:32 -0300

    2to3

--- package/src/setup.py
+++ package/src/setup.py
@@ -5,14 +5,14 @@ from distutils.core import setup
 from definability.interfaces import config
 
 
-print("*" * 80)
+print(("*" * 80))
 print("")
 print("This package needs Minion and LADR.")
-print("Minion path: %s" % config.minion_path)
-print("LADR path: %s" % config.ladr_path)
+print(("Minion path: %s" % config.minion_path))
+print(("LADR path: %s" % config.ladr_path))
 print("If you need to change the path, edit config.py and reinstall.")
 print("")
-print("*" * 80)
+print(("*" * 80))
 
 setup(name='definability',
       version='0.1',
