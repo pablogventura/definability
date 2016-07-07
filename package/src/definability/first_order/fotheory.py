e6be811eae99478ebea448fbdf43d7f949c928a3
Pablo Ventura <pablogventura@gmail.com>
2016-07-07 13:05:38 -0300

    quite syntax de fotheory

--- package/src/definability/first_order/fotheory.py
+++ package/src/definability/first_order/fotheory.py
@@ -8,7 +8,7 @@ class FO_Theory():
     FO_Theories = []
     FirstOrderClasses = []
 
-    def __init__(self, abbr, name, axioms, results=[], options=[], syntax='Prover9'):
+    def __init__(self, abbr, name, axioms, results=[], options=[]):
         """
         Define a first-order class of models by a list of first-order axioms
 
@@ -18,12 +18,9 @@ class FO_Theory():
             axioms  -- list of strings in the given syntax
             results -- list of strings in the given syntax
             options -- list of strings defining the syntax
-            syntax  -- a string indicating which program can parse the
-                       axioms, results and options
         """
         self.abbr = abbr
         self.name = name
-        self.syntax = syntax
         self.axioms = axioms
         self.results = results
         self.options = options
@@ -38,7 +35,7 @@ class FO_Theory():
 
         >>> from definability.first_order import fotheories
         >>> print(fotheories.DLat)
-        FO_Theory("Distributive lattices", syntax="Prover9", axioms=[
+        FO_Theory("Distributive lattices", axioms=[
         "(x v y) v z = x v (y v z)",
         "x v y = y v x",
         "(x^y)^z = x^(y^z)",
@@ -50,8 +47,7 @@ class FO_Theory():
         "x v (y^z) = (x v y)^(x v z)",
         "((x v y)^(x v z))^(y v z) = ((x^y)v(x^z))v(y^z)"])
         """
-        st = 'FO_Theory(\"' + self.name + '\", syntax=\"' + self.syntax +\
-             '\"' + ', axioms=[\n\"' + '\",\n\"'.join(self.axioms) + '\"]'
+        st = 'FO_Theory(\"' + self.name + '\"' + ', axioms=[\n\"' + '\",\n\"'.join(self.axioms) + '\"]'
         if self.options != []:
             st += ',\noptions=[\n\"' + '\",\n\"'.join(self.options) + '\"]'
         if self.results != []:
@@ -65,7 +61,7 @@ class FO_Theory():
         INPUT:
             abbr -- a short name (string) for the new FO subclass
             name -- a string naming the new FO subclass
-            arg -- a list of axioms or an existing FO_Theory using same syntax
+            arg -- a list of axioms or an existing FO_Theory
         """
         if type(arg) != list:
             arg = arg.axioms  # assume its another FO_Theory
