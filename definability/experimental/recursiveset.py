e9e57b08665fc50e69bdb9f5195650ba108232ba
Pablo Ventura <pablogventura@gmail.com>
2016-07-31 16:07:27 -0300

    docstrings

--- definability/experimental/recursiveset.py
+++ definability/experimental/recursiveset.py
@@ -2,6 +2,9 @@ import inspect
 from itertools import count, cycle
 
 class RecursiveSet(object):
+    """
+    Clase para crear conjuntos recursivos con una funcion chi
+    """
     def __init__(self,f,superset):
         self.f = f
         self.superset= superset
@@ -15,6 +18,7 @@ class RecursiveSet(object):
         for i in self.superset:
             if self.f(i):
                 yield i
+                
     def __repr__(self):
         return "{item in %s: %s}" % (self.superset, ("".join(inspect.getsourcelines(self.f)[0])))
     
@@ -22,11 +26,20 @@ class RecursiveSet(object):
         return True
         
 def chiSet(superset):
+    """
+    Decorador para convertir facilmente una funcion
+    en un conjunto, usandola como chi
+    Necesita un generador del superset, para poder
+    iterar sobre el.
+    """
     def wrap(f):
         return RecursiveSet(f,superset)
     return wrap
 
 def enumerateSets(*args):
+    """
+    Dados generadores infinitos, genera la union
+    """
     sets = [iter(s) for s in args]
     for s in cycle(sets):
         yield s.__next__()
@@ -34,10 +47,16 @@ def enumerateSets(*args):
 
 @chiSet(count(0))
 def omega(n):
+    """
+    Ejemplo del conjunto omega
+    """
     return isinstance(n,int) and n>=0
 
 
 
 @chiSet(enumerateSets(count(0),count(-1,-1)))
 def zeta(n):
+    """
+    Ejemplo del conjunto zeta
+    """
     return isinstance(n,int)
