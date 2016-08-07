ef5f551ce9ccc662fd08942678ee145bee92d768
Pablo Ventura <pablogventura@gmail.com>
2016-08-07 17:44:00 -0300

    test para probar el viejo algoritmo contra el nuevo

---
+++ tests/test_oldvsnew.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+from unittest import TestCase
+
+from definability.first_order import fotheories
+from definability.first_order import formulas
+from definability.definability import lindenbaum
+from definability.definability.newconstellation2 import Model_Family
+
+class TestOld(TestCase):
+
+    def setUp(self):
+        self.graphs = fotheories.Graph.find_models(4)
+
+    def test_len(self):
+        self.assertEqual(len(self.graphs), 89) # hay 89 grafos
+    
+    def test_oldnewalgoritms(self):
+        for g in self.graphs:
+            new = sorted(list(formulas.bolsas(g,2).values()))
+            old = sorted([list(d.values())[0] for d in lindenbaum.open_definable_lindenbaum(Model_Family([g]),2,g.fo_type)])
+            self.assertEqual(new, old) # hay 89 grafos
