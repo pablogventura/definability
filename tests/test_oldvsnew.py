485ba1fbaad29eb9a66977093a746517722114c3
Pablo Ventura <pablogventura@gmail.com>
2016-08-22 20:44:21 -0300

    log ahora pone la fecha

--- tests/test_oldvsnew.py
+++ tests/test_oldvsnew.py
@@ -10,7 +10,7 @@ from definability.first_order import formulas
 from definability.definability import lindenbaum
 from definability.definability.newconstellation2 import Model_Family
 
-logging.basicConfig(filename='history.log', level=logging.INFO)
+logging.basicConfig(format='%(asctime)s %(message)s', filename='history.log', level=logging.INFO)
 
 class TestOld(TestCase):
 
