8d25f418f8d5d3880153e48d6f84e6d63fc8c017
Pablo Ventura <pablogventura@gmail.com>
2016-12-12 20:11:44 -0300

    limpiando mas el main

--- main.py
+++ main.py
@@ -6,16 +6,9 @@ from definability import *
 
 def main():
 
-    # First import the embeddable shell class
     from IPython.terminal.embed import InteractiveShellEmbed
 
-    # Now create an instance of the embeddable shell. The first argument is a
-    # string with options exactly as you would type them if you were starting
-    # IPython at the system command line. Any parameters you want to define for
-    # configuration can thus be specified here.
-    ipshell = InteractiveShellEmbed(banner1 = 'Definability\n',
-                                    exit_msg = 'Leaving Definability')
-
+    ipshell = InteractiveShellEmbed(banner1 = 'Definability\n', exit_msg = 'Leaving Definability')
 
     ipshell('Hit Ctrl-D to exit interpreter.\n')
 
