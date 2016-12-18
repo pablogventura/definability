74c7e67e25b7dd01257e0d275705944786177e37
Pablo Ventura <pablogventura@gmail.com>
2016-12-18 18:16:09 -0300

    promedia grafos

--- test_cases/graphics.py
+++ test_cases/graphics.py
@@ -3,20 +3,32 @@
 
 import sqlite3
 import matplotlib.pyplot as plt
+from collections import defaultdict
+import numpy as np
 
 def main():
-    path = input("Path to db file[graphs.db]: ") or "graphs.db"
+    path = "graphs.db"
     conn = sqlite3.connect(path)
 
     c = conn.cursor()
-    c.execute('SELECT * FROM arities WHERE graph_id = 10')
-    arities = []
-    times = []
-    for d in c:
-        arities.append(d[1])
-        times.append(d[3])
+    c.execute("""SELECT * FROM graphs INNER JOIN arities ON graphs.id=arities.graph_id
+                 WHERE graphs.nnodes=3""")
+    ('(1, 0, [0], [])', 0, 1, 0, 0, 1, None, 0, 0, 1, 0.0007132369973987807)
     
+    data=defaultdict(list)
+
+    for d in c:
+        graph,graphid,nnodes,nedges,ncolors,ngensubisos,timegensubisos,_,arity,natomslindenbaum,timealgebra = d
+        data[arity].append(timealgebra)
+
+    arities=list(data.keys())
+    times=[]
+    for arity in arities:
+        times.append(sum(data[arity])/len(data[arity]))
+    arities = np.array(arities)
+    times = np.array(times)
     conn.close()
+    print (arities,times)
     plt.plot(arities, times, '-')
     plt.axis([0, max(arities)+1, 0, max(times)*1.1])
     plt.ylabel('Seconds')
