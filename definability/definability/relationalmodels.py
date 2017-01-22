aa930476f771f35dc6afb727178524059d07acc9
Pablo Ventura <pablogventura@gmail.com>
2017-01-22 15:53:19 -0300

    trabajando en version imperativa

--- definability/definability/relationalmodels.py
+++ definability/definability/relationalmodels.py
@@ -39,5 +39,26 @@ def is_open_def_rel_rec(model, spectre, subtype, supertype, S):
             for aut in subm.automorphisms(subtype):
                 if not aut.preserves_type(supertype):
                     raise Counterexample(iso)
-        is_open_def_rel_rec(subm, spectre, subtype, supertype, S)
+            is_open_def_rel_rec(subm, spectre, subtype, supertype, S)
     return True
+
+def dfs(model, relation):
+    subuniverses = [model.universe]
+    spectre = spectre(relation)
+    while subuniverses:
+        subuniverse = subuniverses.pop()
+        emb,subm = model.substructure(subu, model.fo_type)
+        iso = check_isos(subm, S, subtype)
+        if iso:
+            if not iso.preserves_type(supertype):
+                raise Counterexample(iso)
+        else:
+            S.add(subm)
+            for aut in subm.automorphisms(subtype):
+                if not aut.preserves_type(supertype):
+                    raise Counterexample(iso)
+            size = spectre[0] #TODO ACA HAY QUE TOMAR EL SIGUIENTE EN EL ESPECTRO QUE SEA MAS CHICO QUE LEN DE SUBUNIVERSE
+            subuniverses.extend(list(combinations(subuniverse,size)))
+    return visited
+
+dfs(graph, 'A') # {'E', 'D', 'F', 'A', 'C', 'B'}
