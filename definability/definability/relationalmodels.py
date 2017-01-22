08c1009c003b56f73d9ebc0e58fd1ea0b273a859
Pablo Ventura <pablogventura@gmail.com>
2017-01-22 16:30:09 -0300

    version imperativa

--- definability/definability/relationalmodels.py
+++ definability/definability/relationalmodels.py
@@ -8,7 +8,7 @@ def check_isos(a, s, subtype):
         if iso:
             return iso
 
-def spectre(relation):
+def calc_spectre(relation):
     result = set(len(set(t)) for t in relation)
     result = list(result)
     result.sort(reverse=True)
@@ -19,7 +19,7 @@ def is_open_def_rel(model, relation):
     model.relations["R"] = relation
     subtype = model.fo_type
     model.fo_type = model.fo_type + FO_Type({},{"R":relation.arity()})
-    return is_open_def_rel_rec(model, spectre(relation), subtype, model.fo_type, set())
+    return is_open_def_rel_rec(model, calc_spectre(relation), subtype, model.fo_type, set())
 
 
 def is_open_def_rel_rec(model, spectre, subtype, supertype, S):
@@ -38,16 +38,26 @@ def is_open_def_rel_rec(model, spectre, subtype, supertype, S):
             S.add(subm)
             for aut in subm.automorphisms(subtype):
                 if not aut.preserves_type(supertype):
-                    raise Counterexample(iso)
+                    raise Counterexample(aut)
             is_open_def_rel_rec(subm, spectre, subtype, supertype, S)
     return True
 
 def dfs(model, relation):
-    subuniverses = [model.universe]
-    spectre = spectre(relation)
+    model.relations["R"] = relation
+    subtype = model.fo_type
+    model.fo_type = model.fo_type + FO_Type({},{"R":relation.arity()})
+    supertype = model.fo_type
+    
+    
+    spectre = calc_spectre(relation)
+    size = spectre[0]
+
+    subuniverses = list(combinations(model.universe,size))
+    
+    S = set()
     while subuniverses:
         subuniverse = subuniverses.pop()
-        emb,subm = model.substructure(subu, model.fo_type)
+        emb,subm = model.substructure(subuniverse, model.fo_type)
         iso = check_isos(subm, S, subtype)
         if iso:
             if not iso.preserves_type(supertype):
@@ -56,9 +66,12 @@ def dfs(model, relation):
             S.add(subm)
             for aut in subm.automorphisms(subtype):
                 if not aut.preserves_type(supertype):
-                    raise Counterexample(iso)
-            size = spectre[0] #TODO ACA HAY QUE TOMAR EL SIGUIENTE EN EL ESPECTRO QUE SEA MAS CHICO QUE LEN DE SUBUNIVERSE
-            subuniverses.extend(list(combinations(subuniverse,size)))
-    return visited
+                    raise Counterexample(aut)
+            try:
+                size = next(x for x in spectre if x < len(subuniverse)) # EL SIGUIENTE EN EL ESPECTRO QUE SEA MAS CHICO QUE LEN DE SUBUNIVERSE
+                subuniverses.extend(list(combinations(subuniverse,size)))
+            except StopIteration:
+                # no tiene mas hijos
+                pass
+    return True
 
-dfs(graph, 'A') # {'E', 'D', 'F', 'A', 'C', 'B'}
